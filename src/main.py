28af184179b1a6e1b4c48a31dae5db524df50d2d
sakdag <serhat.akdag@outlook.com>
2021-05-08 23:52:55 +0300

    Understanding and preparing dataset (unfinished)

--- src/main.py
+++ src/main.py
@@ -2,4 +2,21 @@ import understanding_dataset as ud
 
 
 if __name__ == "__main__":
-    ud.read_dataset("crime-data-from-2010-to-present.csv")
+    # df = ud.read_dataset("crime-data-from-2010-to-present.csv")
+    # df = ud.read_dataset("Crime_Data_2010_2017.csv")
+    #
+    # column_list = {"Date Reported", "Date Occurred", "Area ID", "Area Name",
+    #                "Reporting District", "MO Codes", "Weapon Used Code",
+    #                "Weapon Description", "Status Code", "Status Description",
+    #                "Crime Code 1", "Crime Code 2", "Crime Code 3", "Crime Code 4",
+    #                "Address", "Cross Street"}
+    # df = df.drop(columns=column_list)
+    #
+    # filtered_csv = df.to_csv(r'crime_data_2010_2017_filtered.csv', index=False)
+
+    df = ud.read_dataset("crime_data_2010_2017_filtered.csv")
+
+    ud.categorize_time_occurred(df)
+    print(df['Time Occurred'].unique())
+
+    # ud.fill_with_average(df, 'Victim Age', True)
--- src/understanding_dataset.py
+++ src/understanding_dataset.py
@@ -1,4 +1,5 @@
 import math
+
 import pandas as pd
 
 
@@ -6,29 +7,78 @@ def read_dataset(path):
     df = pd.read_csv(path)
     df_size = len(df)
     print("Data Size: " + str(df_size))
+    return df
 
 
 def fill_with_average(df, column_name, is_round):
     df_size = len(df)
     target_column = df[column_name]
-    nan_list = []
-    total = 0
-    for i in range(0, df_size):
-        value = target_column[i]
-        if math.isnan(value):
-            nan_list.append(i)
-        else:
-            total = total + value
+    nan_list, value_list = get_nan_valid_tuple(df, column_name)
 
-    avg = total / (df_size - len(nan_list))
+    avg = math.fsum(value_list) / (df_size - len(nan_list))
     if is_round:
         avg = math.ceil(avg)
 
     for i in nan_list:
         target_column[i] = avg
 
-    for i in range(0, df_size):
-        value = df[column_name][i]
-        if math.isnan(value):
-            print("You copied, silly!")
 
+def fill_with_median(df, column_name, is_round):
+    target_column = df[column_name]
+    nan_list, value_list = get_nan_valid_tuple(df, column_name)
+
+    value_list.sort()
+    size = len(value_list)
+
+    if size % 2 == 0:
+        median = (value_list[int(size / 2) - 1] + value_list[int(size / 2)]) / 2
+        if is_round:
+            median = math.ceil(median)
+    else:
+        median = value_list[math.floor(size / 2)]
+
+    for i in nan_list:
+        target_column[i] = median
+
+
+def get_nan_valid_tuple(df, column_name):
+    df_size = len(df)
+    target_column = df[column_name]
+    nan_list = []
+    value_list = []
+
+    for i in range(0, df_size):
+        value = target_column[i]
+        if math.isnan(value):
+            nan_list.append(i)
+        else:
+            value_list.append(i)
+    return nan_list, value_list
+
+
+def fill_with_median_2(df, column_name):
+    target_column = df[column_name]
+    indexes = pd.isnull(df).any(1).nonzero()[0]
+    median = pd.DataFrame.median(df)
+    for i in indexes:
+        target_column[i] = median
+
+
+def fill_with_mean_2(df, column_name):
+    target_column = df[column_name]
+    indexes = pd.isnull(df).any(1).nonzero()[0]
+    mean = pd.DataFrame.mean(df)
+    for i in indexes:
+        target_column[i] = mean
+
+
+def fill_with_knn(df, column_name, is_round):
+    print("Not implemented yet")
+
+
+def categorize_time_occurred(df: pd.DataFrame):
+    df['Time Occurred'] = df['Time Occurred'].floordiv(300)
+
+
+def create_geolocation_columns(df: pd.DataFrame):
+    print("Not implemented yet")
